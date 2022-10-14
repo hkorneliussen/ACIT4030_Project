a96c371967aa5d2421f45abc970da61bbc46c0ac
hkorneliussen <99089340+hkorneliussen@users.noreply.github.com>
2022-10-14 12:46:11 +0200

    Update colorize.py

--- colorize.py
+++ colorize.py
@@ -1,4 +1,23 @@
+'''
+This script adds color to the input image. The script is based on: 
 
+@inproceedings{zhang2016colorful,
+  title={Colorful Image Colorization},
+  author={Zhang, Richard and Isola, Phillip and Efros, Alexei A},
+  booktitle={ECCV},
+  year={2016}
+}
+
+@article{zhang2017real,
+  title={Real-Time User-Guided Image Colorization with Learned Deep Priors},
+  author={Zhang, Richard and Zhu, Jun-Yan and Isola, Phillip and Geng, Xinyang and Lin, Angela S and Yu, Tianhe and Efros, Alexei A},
+  journal={ACM Transactions on Graphics (TOG)},
+  volume={9},
+  number={4},
+  year={2017},
+  publisher={ACM}
+}
+'''
 import argparse
 import matplotlib.pyplot as plt
 
