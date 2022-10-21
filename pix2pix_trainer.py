6045e5f87422f8012de29f22a6f6f6cbcc6757a5
hkorneliussen <99089340+hkorneliussen@users.noreply.github.com>
2022-10-21 11:54:11 +0200

    Update pix2pix_trainer.py

--- pix2pix_trainer.py
+++ pix2pix_trainer.py
@@ -44,8 +44,14 @@ for path in paths:
   if not isExist:
     os.makedirs(path)
 
-#defining helper functions
+dataset_path = '/content/ACIT4030_Project/pix2pix_training/image_dataset'
+isExists = os.path.exists(dataset_path)
+if not isExist:
+  import kaggle
+  kaggle.api.authenticate()
+  kaggle.api.dataset_download_files('hannekorneliussen/pix2pix-dataset', path='/content/ACIT4030_Project/pix2pix_training', unzip=True)
 
+#defining helper functions
 def load(image_file):
   # Read and decode an image file to a uint8 tensor
   image = tf.io.read_file(image_file)
@@ -118,20 +124,284 @@ def load_image_test(image_file):
 
   return input_image, real_image
 
-print('loading dataset')
-
-
-
-test_images_folder = "/global/D1/homes/hanne/2D_Face_generating/dataset/image_dataset/test/"
-train_images_folder = "/global/D1/homes/hanne/2D_Face_generating/dataset/image_dataset/train/"
-val_images_folder = "/global/D1/homes/hanne/2D_Face_generating/dataset/image_dataset/val/"
-
+test_images_folder = "/content/ACIT4030_Project/pix2pix_training/image_dataset/test/"
+train_images_folder = "/content/ACIT4030_Project/pix2pix_training/image_dataset/train/"
+val_images_folder = "/content/ACIT4030_Project/pix2pix_training/image_dataset/val/"
 
 test_images = os.listdir(test_images_folder)
 train_images = os.listdir(train_images_folder)
 val_images = os.listdir(val_images_folder)
 
-print('number of test images: ' + str(len(test_images)))
-print('number of train images: ' + str(len(train_images)))
-print('number of validation images: ' + str(len(val_images)))
+#loading dataset into train_dataset variable and test_dataset variable
 
+path_dataset = "/content/ACIT4030_Project/pix2pix_training/image_dataset"
+PATH  = pathlib.Path(path_dataset)
+print(PATH)
+
+#PATH = path_to_zip.parent/dataset_name
+
+list(PATH.parent.iterdir())
+
+
+train_dataset = tf.data.Dataset.list_files(str(PATH / 'train/*.jpg'))
+train_dataset = train_dataset.map(load_image_train,
+                                  num_parallel_calls=tf.data.AUTOTUNE)
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.batch(BATCH_SIZE)
+
+try:
+  test_dataset = tf.data.Dataset.list_files(str(PATH / 'test/*.jpg'))
+except tf.errors.InvalidArgumentError:
+  test_dataset = tf.data.Dataset.list_files(str(PATH / 'val/*.jpg'))
+test_dataset = test_dataset.map(load_image_test)
+test_dataset = test_dataset.batch(BATCH_SIZE)
+
+
+# defining up and down sampler models 
+
+def downsample(filters, size, apply_batchnorm=True):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+      tf.keras.layers.Conv2D(filters, size, strides=2, padding='same', 
+                             kernel_initializer=initializer, use_bias=False)
+  )
+
+  if apply_batchnorm:
+    result.add(tf.keras.layers.BatchNormalization())
+
+  result.add(tf.keras.layers.LeakyReLU())
+
+  return result 
+
+def upsample(filters, size, apply_dropout=False):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+      tf.keras.layers.Conv2DTranspose(filters, size, strides=2, padding='same', 
+                             kernel_initializer=initializer, use_bias=False)
+  )
+
+  result.add(tf.keras.layers.BatchNormalization())
+
+  if apply_dropout:
+    result.add(tf.keras.layers.Dropout(0.5))
+
+  result.add(tf.keras.layers.ReLU())
+
+  return result 
+  
+
+# defining generator model 
+
+def Generator():
+  inputs = tf.keras.layers.Input(shape=[256, 256, 1])
+
+  down_stack = [
+      downsample(64, 4, apply_batchnorm=False), # (batch size, 128, 128, 64)
+      downsample(128, 4),                       # (batch_size, 64, 64, 128)
+      downsample(256, 4),                       # (batch_size, 32, 32, 256)
+      downsample(512, 4),                       # (batch_size, 16, 16, 512)
+      downsample(512, 4),                       # (batch_size, 8, 8, 512)
+      downsample(512, 4),                       # (batch_size, 4, 4, 512)
+      downsample(512, 4),                       # (batch_size, 2, 2, 513)
+      downsample(512, 4),                       # (batch_size, 1, 1, 512)
+  ]
+
+  up_stack = [
+      upsample(512, 4, apply_dropout=True),     # (batch_size, 2, 2, 1024)
+      upsample(512, 4, apply_dropout=True),     # (batch_size, 4, 4, 1024)
+      upsample(512, 4, apply_dropout=True),     # (batch_size_ 8,8, 1024)
+      upsample(512, 4),                         # (batch_size, 16, 16, 1034)
+      upsample(256, 4),                         # (batch_size, 32, 32, 512)
+      upsample(128, 4),                         # (batch_size, 64, 64, 256)
+      upsample(64, 4),                          # (batch_size, 128, 128, 128)
+  ]
+
+  initializer = tf.random_normal_initializer(0.0, 0.02)
+  last = tf.keras.layers.Conv2DTranspose(
+      OUTPUT_CHANNELS, 4, strides=2, padding='same',
+      kernel_initializer=initializer, activation='tanh'   # (batch_size, 256, 256, 3)
+  )
+
+  x = inputs 
+
+  # downsampling through the model 
+  skips = []
+  for down in down_stack:
+    x = down(x)
+    skips.append(x)
+
+  skips = reversed(skips[:-1])
+
+  #upsampling and establishing the skip connections 
+  for up, skip in zip(up_stack, skips):
+    x = up(x)
+    x = tf.keras.layers.Concatenate()([x, skip])
+  
+  x = last(x)
+
+  return tf.keras.Model(inputs=inputs, outputs=x)
+  
+# creating generator model 
+
+generator = Generator()
+
+# defining discriminator model 
+
+def Discriminator():
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  inp = tf.keras.layers.Input(shape=[256, 256, 1], name='input_image')
+  tar = tf.keras.layers.Input(shape=[256, 256, 1], name='target_image')
+
+  x = tf.keras.layers.concatenate([inp, tar])  # (batch_size, 256, 256, channels*2)
+
+  down1 = downsample(64, 4, False)(x)  # (batch_size, 128, 128, 64)
+  down2 = downsample(128, 4)(down1)  # (batch_size, 64, 64, 128)
+  down3 = downsample(256, 4)(down2)  # (batch_size, 32, 32, 256)
+
+  zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)  # (batch_size, 34, 34, 256)
+  conv = tf.keras.layers.Conv2D(512, 4, strides=1,
+                                kernel_initializer=initializer,
+                                use_bias=False)(zero_pad1)  # (batch_size, 31, 31, 512)
+
+  batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+
+  leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
+
+  zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  # (batch_size, 33, 33, 512)
+
+  last = tf.keras.layers.Conv2D(1, 4, strides=1,
+                                kernel_initializer=initializer)(zero_pad2)  # (batch_size, 30, 30, 1)
+
+  return tf.keras.Model(inputs=[inp, tar], outputs=last)
+  
+# creating discriminator model 
+
+discriminator = Discriminator()
+
+
+# defining loss functions 
+loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def generator_loss(disc_generated_output, gen_output, target): 
+  gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
+
+  # mean absolute error
+  l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+
+  total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+
+  return total_gen_loss, gan_loss, l1_loss
+
+def discriminator_loss(disc_real_output, disc_generated_output):
+  real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
+
+  generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
+
+  total_disc_loss = real_loss + generated_loss 
+
+  return total_disc_loss
+  
+
+# defining optimizers and a checkpoint saver , and summary writer
+
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer, 
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+summary_writer = tf.summary.create_file_writer(
+  log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+  
+
+# defining function for generating images 
+
+def generate_images(model, test_input, tar, iter):
+  prediction = model(test_input, training=True)
+  plt.figure(figsize=(15, 15))
+
+  display_list = [test_input[0], tar[0], prediction[0]]
+  title = ['input image', 'ground truth', 'predicted image']
+
+  for i in range(3):
+    plt.subplot(1,3,i+1)
+    plt.title(title[i])
+    # getting pixel values in the [0,1] range to plot 
+    plt.imshow(display_list[i][:,:,0] * 0.5 + 0.5, cmap='gray')
+    plt.axis('off')
+
+  #plt.show()
+  
+  name = result_dir + '/' + str(iter) + '.jpg'
+  plt.savefig(name)
+  plt.close()
+
+# defining training function 
+@tf.function
+def train_step(input_image, target, step):
+  with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    gen_output = generator(input_image, training=True)
+
+    disc_real_output = discriminator([input_image, target], training=True)
+    disc_generated_output = discriminator([input_image, gen_output], training=True)
+
+    gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_output, target)
+    disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+    print(gen_total_loss)
+
+    generator_gradients = gen_tape.gradient(gen_total_loss, generator.trainable_variables)
+
+    discriminator_gradients = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(generator_gradients, 
+                                            generator.trainable_variables))
+    
+    discriminator_optimizer.apply_gradients(zip(discriminator_gradients, 
+                                                discriminator.trainable_variables))
+    
+    with summary_writer.as_default():
+      tf.summary.scalar('gen_total_loss', gen_total_loss, step=step//1000)
+      tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=step//1000)
+      tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=step//1000)
+      tf.summary.scalar('disc_loss', disc_loss, step=step//1000)
+
+# defining training loop function 
+def fit(train_ds, test_ds, steps):
+  example_input, example_target = next(iter(test_ds.take(1)))
+  start = time.time()
+
+  for step, (input_image, target) in train_ds.repeat().take(steps).enumerate():
+    if (step) % 5 == 0:
+      display.clear_output(wait=True)
+
+      if step!=0:
+        print(f'Time taken for 5 steps: {time.time()-start:.2f} sec\n')
+
+      start = time.time()
+
+      generate_images(generator, example_input, example_target)
+      print(f"Step: {step//1000}k")
+
+    train_step(input_image, target, step)
+
+    #training step
+    if (step+1) % 1 == 0:
+      print('.', end='', flush=True)
+
+    # save (checkpoint) the model every 5k steps 
+    if (step+1) % 10 == 0:
+      checkpoint.save(file_prefix=checkpoint_prefix)
+
+#running the training loop
+fit(train_dataset, test_dataset, steps=20)
+
+for inp, tar in test_dataset.take(5):
+  generate_images(generator, inp, tar)
